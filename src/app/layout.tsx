2621d94a76346e391d4a07bccd2f496845872849
Misha <utika.nice@gmail.com>
2024-01-25 23:29:20 +0300

    Added a variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], display: 'swap', variable: '--font-inter'});
 
 export const metadata: Metadata = {
   title: "Mineplace",
