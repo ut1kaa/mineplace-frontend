5a3736d925cd3469f92c256f7f34e84dc989acf4
Misha <utika.nice@gmail.com>
2024-01-25 23:28:47 +0300

    Made the button animations, and finished making the outlineButton

--- src/components/ui/buttons.tsx
+++ src/components/ui/buttons.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/components/ui/buttons.module.scss";
+import virables from "@/styles/exportedVirables.module.scss";
 
 type DarkButtonProps = {
     text: string;
@@ -15,7 +16,16 @@ type DarkButtonProps = {
 const OutlineButton: React.FC<DarkButtonProps> = ({ text }) => {
     return (
         <button className={styles.outlineButton}>
-            {text}
+            <svg width="170" height="48">
+                <defs>
+                    <linearGradient id="outline-button-gradient">
+                        <stop offset="0%" stopColor={virables.outlineButton_gradient_start}/>
+                        <stop offset="100%" stopColor={virables.outlineButton_gradient_stop} />
+                    </linearGradient>
+                </defs>
+                <rect x="1" y="1" rx="18" fill="none" stroke="url(#outline-button-gradient)" width="168" height="36"></rect>
+            </svg>
+            <span>{text}</span>
         </button>
     );
 }
