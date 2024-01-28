07bf6c550f836112519a36f10dc82cf04729eee6
Misha <utika.nice@gmail.com>
2024-01-28 19:52:24 +0300

    The main part of the index page is made

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,34 @@
+import { GradientButton, OutlineButton } from "@/components/ui/buttons";
+import { Logo } from "@/components/label/Logo";
+
+import MainPattern from  '$/assets/patterns/MainBackground.svg'
+
 import styles from "@/styles/(main)/page.module.scss";
+import TextAnimation from "@/components/label/textAnimation";
 
 export default function Home() {
   return (
-    <>Hello world!</>
+    <>
+      <div className={styles.main}>
+        <div className={styles.content}>
+          <span>
+            <h2>Здесь можно найти</h2>
+            <h1>Minecraft</h1>
+          </span>
+          <h2 className={styles.animatetedLabel}><TextAnimation/></h2>
+          <p>Играй, делись, изучай Minecraft дополнения
+            через нашу открытую платформу созданную для сообщества.
+          </p>
+          <GradientButton text="Исследовать"/>
+          <OutlineButton text="Создать аккаунт"/>
+        </div>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <div className={styles.mainParrern}>
+          <MainPattern/>
+        </div>
+      </div>
+    </>
   );
 }
