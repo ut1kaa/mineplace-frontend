8b347133495a20f88ae27b10e3db2b10835c5e7c
Misha <utika.nice@gmail.com>
2024-01-28 19:52:53 +0300

    Animated text from the home page

---
+++ src/components/label/textAnimation.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import styles from '@/styles/components/label/textAnimation.module.scss';
+
+const TextAnimation = () => {
+    const texts = ["Моды", "Плагины", "Датапаки", "Шейдеры", "Ресурспаки"];
+    const [index, setIndex] = useState(0);
+    const [isPageLoaded, setPageLoaded] = useState(false);
+    const [isFirstSlide, setFirstSlide] = useState(true);
+
+    useEffect(() => {
+        setPageLoaded(true);
+
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % texts.length);
+            setFirstSlide(false);
+        }, 3000);
+
+        return () => clearInterval(interval);
+        
+    }, []);
+
+    return (
+      <div className={styles.container}>
+        <div className={`${styles.text} ${isPageLoaded ? (isFirstSlide ? styles.firstAnimation : styles.Animation) : ''}`}>
+            {texts[index]}
+        </div>
+      </div>
+    );
+  };
+  
+  export default TextAnimation;
