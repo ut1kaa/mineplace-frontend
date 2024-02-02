056e63f361b2441ee1d1a44a063db5bfec24e439
Misha <utika.nice@gmail.com>
2024-02-02 23:25:52 +0300

    work on category and for players block

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import { GradientButton, OutlineButton } from "@/components/ui/buttons";
 import { Logo } from "@/components/label/Logo";
+import { OutlineBlock } from "@/components/ui/outlineBlock";
 
 import MainPattern from  '$/assets/patterns/MainBackground.svg'
 
@@ -46,6 +47,63 @@ export default function Home() {
               <h1>0.3%</h1>
               <p>Плохих отзывов</p>
             </section>
+          </div>
+        </div>
+      </div>
+      <div className={styles.categories_layout}>
+          <p>Популярные</p>
+          <h1>категории</h1>
+          <div className={styles.categories}>
+
+        </div>
+      </div>
+      <div className={styles.players_section}>
+        <div className={styles.label}>
+          <span>Для игроков</span>
+        </div>
+        <div className={styles.label_info}>
+          <h1>Изучи остальные дополнения</h1>
+          <p>Будь уверен, что сможешь найти любое дополнение:
+             от обычных биомов до мистических модов -
+              которые смогут вывести вашу игру на новый уровень.</p>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.block}>
+            <div className={styles.preview}>
+              <OutlineBlock>
+                <div className={styles.search_content}>
+                  <p>Прикол</p>
+                </div>
+              </OutlineBlock>
+            </div>
+            <div className={styles.info}>
+              <h1>Ищи то, что тебе нужно, быстро и легко</h1>
+              <p>Mineplace позволяет использовать молниеностный поиск и мощные фильтры которые помогут найти любое дополнение.</p>
+            </div>
+          </div>
+          <div className={styles.block}>
+            <div className={styles.info}>
+              <h1>Следи за любимыми дополнениями</h1>
+              <p>Получай уведомления каждый раз когда избранные дополнения обновляются.</p>
+            </div>
+            <div className={styles.preview}>
+              <OutlineBlock>
+                  <div className={styles.follow_content}>
+                  </div>
+                </OutlineBlock>
+            </div>
+          </div>
+          <div className={styles.block}>
+            <div className={styles.preview}>
+              <OutlineBlock>
+                <div className={styles.support_content}>
+                </div>
+              </OutlineBlock>
+            </div>
+            <div className={styles.info}>
+              <h1>Общайся и получай поддержку</h1>
+              <p>Обсуждай любое дополнение с сообществом и получай поддержку от разработчиков.</p>
+            </div>
           </div>
         </div>
       </div>
