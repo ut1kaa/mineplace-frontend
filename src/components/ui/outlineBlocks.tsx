3fb9d4f75aa72e6d53a0a828c42842c993cf403a
Misha <utika.nice@gmail.com>
2024-02-04 12:36:53 +0300

    add outline sub block

--- src/components/ui/outlineBlocks.tsx
+++ src/components/ui/outlineBlocks.tsx
@@ -1,6 +1,6 @@
 import {addRandomChars} from "@/utils/idGenerator";
 
-import styles from "@/styles/components/ui/outlineBlock.module.scss";
+import styles from "@/styles/components/ui/outlineBlocks.module.scss";
 import virables from "@/styles/exportedVirables.module.scss";
 
 const OutlineBlock = ({ children }: { children: React.ReactNode; }) => {
@@ -25,4 +25,27 @@ const OutlineBlock = ({ children }: { children: React.ReactNode; }) => {
     );
 }
 
-export {OutlineBlock};
+
+const OutlineSubBlock = ({children, style}: {children: React.ReactNode; style?: React.CSSProperties }) => {
+    const elementId = addRandomChars("outline-subblock-gradient");
+    const url = `url(#${elementId})`;
+
+    return (
+        <div className={styles.outlineSubBlock} style={style}>
+        <svg width="100%" height="100%">
+            <defs>
+                <linearGradient id={elementId} gradientTransform="rotate(90) translate(0, 0)">
+                    <stop offset="0%" stopColor={virables.block_outline_gradient_start}/>
+                    <stop offset="100%" stopColor={virables.block_outline_gradient_stop}/>
+                </linearGradient>
+            </defs>
+            <rect x="0" y="0" rx="25" fill="none" stroke={url} width="100%" height="100%"></rect>
+        </svg>
+        <div className={styles.content}>
+            {children}
+        </div>
+    </div>
+    );
+}
+
+export {OutlineBlock, OutlineSubBlock};
