b0d5cf56636ff8f0898820638372e8ed4c51f93b
Misha <utika.nice@gmail.com>
2024-02-21 22:55:25 +0300

    edit old buttons to Custom

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,15 +2,42 @@
 import { useEffect, useState } from "react";
 import styles from '@/styles/components/ui/header.module.scss'
 import { Logo } from "@/components/label/Logo";
-import { DarkLightButton} from "@/components/ui/buttons";
+import { CustomButton } from "@/components/ui/buttons";
+
+import { InitNavBar } from "@/components/ui/navBar";
 
 import SeachIcon from "$/assets/icons/search.svg"
 import BurgerIcon from "$/assets/icons/burger.svg"
 import CrossIcon from "$/assets/icons/cross.svg"
 
+import virables from "@/styles/exportedVirables.module.scss";
+
 const MobileHeader = () => {
     const [showSearch, setShowSearch] = useState<boolean>(false);
     const [showBurger, setShowBurger] = useState<boolean>(false);
+
+    const test_links = [
+        {
+          name: "Моды",
+          to: "/mods",
+        },
+        {
+          name: "Плагины",
+          to: "/plugins",
+        },
+        {
+          name: "Датапаки",
+          to: "/datapacks",
+        },
+        {
+          name: "Шейдеры",
+          to: "/shaders",
+        },
+        {
+          name: "Ресурспаки",
+          to: "/resourcepacks",
+        },
+      ];
 
     return (
         <div className={styles.mobile_header}>
@@ -40,12 +67,12 @@ const MobileHeader = () => {
                 </div>
             </div>
             <div className={`${styles.burgerTab} ${showBurger === true ? styles.activeTab : styles.inactiveTab}`}>
-                <DarkLightButton text="Войти" redirectTo="/singIn"/>
-                <DarkLightButton redirectTo="/singUp" text="Создать аккаунт"/>
+                <CustomButton colors={{background_color: virables.lightButton_hex, text_color: virables.lightButton_text_hex}} text="Войти" redirectTo="/singIn"/>
+                <CustomButton colors={{background_color: virables.darkLightButton_hex, text_color: virables.darkLightButton_text_hex}} redirectTo="/singUp" text="Создать аккаунт"/>
             </div>
 
-            <div className={`${styles.searchTab} ${showSearch === true ? styles.activeTab : styles.inactiveTab}`}>
-               поиск 
+            <div className={`${styles.searchTab} ${showSearch === true ? styles.activeTabSearch : styles.inactiveTab}`}>
+                <InitNavBar navList={test_links} activeHighligth={true} mobile={true}/>
             </div>
           </div>
     );
